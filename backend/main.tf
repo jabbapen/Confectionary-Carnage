f15389626770870fc33210be85674f9796ba9a1e
jabbapen <jabbapen@gmail.com>
2024-10-17 22:44:33 -0700

    update github action

--- backend/main.tf
+++ backend/main.tf
@@ -30,7 +30,7 @@ resource "null_resource" "image" {
 
   provisioner "local-exec" {
     command = <<EOF
-      aws ecr get-login-password | docker login --username AWS --password-stdin ${local.repo_url}
+      aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin ${local.repo_url}
       docker build --platform linux/amd64 -t ${local.repo_url}:latest .
       docker push ${local.repo_url}:latest
     EOF
