d5b5ff2c371197589c42cb4f1385b11736cfd9c2
Laura Ness <lness@g.ucla.edu>
2024-11-28 21:44:41 -0800

    Levels API

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,11 @@ class LeaderboardModel(BaseModel):
     name: str
     score: int
 
+class LevelModel(BaseModel):
+    level_name: str
+    author: str
+    serialized_level: str
+
 # added to minimize code bloat
 def get_db_conn():
     try: 
@@ -32,19 +37,36 @@ def init_leaderboard(conn):
             leaderboard_query = """
             CREATE TABLE IF NOT EXISTS leaderboard (
                 id SERIAL PRIMARY KEY,
-                name varchar(100) NOT NULL,
-                score int NOT NULL
+                name VARCHAR(100) NOT NULL,
+                score INT NOT NULL
             );
             """
             cur.execute(leaderboard_query)
             conn.commit() 
     except Exception: 
         raise HTTPException(400, "Failed to create leaderboard table")
+    
+def init_levels(conn): 
+    try:
+        with conn.cursor() as cur:
+            level_query = """
+            CREATE TABLE IF NOT EXISTS levels (
+                id SERIAL PRIMARY KEY,
+                level_name VARCHAR(255) NOT NULL,
+		        author VARCHAR(255),
+                serialized_level TEXT NOT NULL
+            );
+            """
+            cur.execute(level_query)
+            conn.commit() 
+    except Exception: 
+        raise HTTPException(400, "Failed to create level table")
 
 @app.on_event("startup")
 def startup():
     conn = get_db_conn()
     init_leaderboard(conn)
+    init_levels(conn)
     conn.close()
 
 @app.get("/")
@@ -103,6 +125,56 @@ async def add_to_leaderboard(entry: LeaderboardModel):
             conn.commit()
 
             return {"statusCode": 200, "message": "Entry added to leaderboard table successfully.", "data": entry}
+    except psycopg2.Error:
+        conn.rollback()
+        raise HTTPException(500, "DB Connection Error")
+    except Exception: 
+        conn.rollback()
+        raise HTTPException(400, "Generic error")
+    finally:
+        if conn is not None:
+            conn.close()
+
+# return first limit entries in levels
+@app.get("/levels")
+async def get_levels(limit=None):
+    conn = get_db_conn()
+    try:
+        with conn.cursor() as cur:
+            query = """
+                SELECT level_name, author, serialized_level
+                FROM levels
+            """
+            if limit is not None:
+                query += " LIMIT %s"
+                cur.execute(query, (limit,))
+            else:
+                cur.execute(query)
+            result = cur.fetchall()
+            levels = [{"level_name": row[0], "author": row[1], "serialized_level": row[2]} for row in result]  
+            return {"statusCode": 200, "message": "Fetched levels successfully", "data": levels}
+    except psycopg2.Error:
+        raise HTTPException(500, "DB Connection Error")
+    except Exception: 
+        raise HTTPException(400, "Generic error")
+    finally:
+        if conn is not None:
+            conn.close()
+
+# add item to levels
+@app.post("/levels")
+async def add_levels(entry: LevelModel):
+    conn = get_db_conn() 
+    try:
+        with conn.cursor() as cur:
+            query = """
+                INSERT INTO levels (level_name, author, serialized_level)
+                VALUES (%s, %s, %s)
+            """
+            cur.execute(query, (entry.level_name, entry.author, entry.serialized_level))
+            conn.commit()
+
+            return {"statusCode": 200, "message": "Entry added to levels table successfully.", "data": entry}
     except psycopg2.Error:
         conn.rollback()
         raise HTTPException(500, "DB Connection Error")
