1c534ba58cbc8d0792a283b8e884144837e2a9a3
Do3naut <51458829+Do3naut@users.noreply.github.com>
2024-12-01 22:29:12 -0800

    Update build settings, link scenes

--- game/Assets/Scripts/GameManager.cs
+++ game/Assets/Scripts/GameManager.cs
@@ -30,13 +30,19 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(levelRequester.QueryAllLevels());
+        StartCoroutine(SetupSystems());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SetupSystems()
+    {
+        yield return levelRequester.QueryAllLevels();
+        SetupLevel();
     }
 
     public void SetupLevel()
